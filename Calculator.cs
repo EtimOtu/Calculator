dbaadbff1a8ec0525ee60b6fc001ed6c3d5e5f94
Etim I Otu <eotu1@student.gsu.edu>
2023-01-28 17:58:21 -0500

    Calculator
    A calculator App made in c#

---
+++ Calculator.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        internal static void Addition()
+        {
+            Console.WriteLine("Addition Selected");
+            Console.WriteLine("Please enter the first number.");
+            var num1 = Console.ReadLine();
+            while (!double.TryParse(num1, out _))
+            {
+                Console.WriteLine("Please enter a valid number");
+                num1 = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{num1} is the first number.");
+            Console.WriteLine("Please enter the second number");
+            var num2 = Console.ReadLine();
+            while(!double.TryParse(num2, out _))
+            {
+                Console.WriteLine("Please enter a valid number");
+                num2 = Console.ReadLine();
+            }
+            Console.WriteLine($"{num2} is the second number");
+            var result = Convert.ToDouble(num1) + Convert.ToDouble(num2);
+            Console.WriteLine($"{num1} + {num2} is {result}");
+            Console.WriteLine("Press Enter to Continue");
+            Console.ReadLine();
+        }
+
+        internal static void Subtraction()
+        {
+            Console.WriteLine("Subtraction Selected");
+            Console.WriteLine("Please enter the first number.");
+            var num1 = Console.ReadLine();
+            while (!double.TryParse(num1, out _))
+            {
+                Console.WriteLine("Please enter a valid number");
+                num1 = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{num1} is the first number.");
+            Console.WriteLine("Please enter the second number");
+            var num2 = Console.ReadLine();
+            while (!double.TryParse(num2, out _))
+            {
+                Console.WriteLine("Please enter a valid number");
+                num2 = Console.ReadLine();
+            }
+            Console.WriteLine($"{num2} is the second number");
+            var result = Convert.ToDouble(num1) - Convert.ToDouble(num2);
+            Console.WriteLine($"{num1} - {num2} is {result}");
+            Console.WriteLine("Press Enter to Continue");
+            Console.ReadLine();
+        }
+
+        internal static void Multiplication()
+        {
+            Console.WriteLine("Multiplication Selected");
+            Console.WriteLine("Please enter the first number.");
+            var num1 = Console.ReadLine();
+            while (!double.TryParse(num1, out _))
+            {
+                Console.WriteLine("Please enter a valid number");
+                num1 = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{num1} is the first number.");
+            Console.WriteLine("Please enter the second number");
+            var num2 = Console.ReadLine();
+            while (!double.TryParse(num2, out _))
+            {
+                Console.WriteLine("Please enter a valid number");
+                num2 = Console.ReadLine();
+            }
+            Console.WriteLine($"{num2} is the second number");
+            var result = Convert.ToDouble(num1) * Convert.ToDouble(num2);
+            Console.WriteLine($"{num1} * {num2} is {result}");
+            Console.WriteLine("Press Enter to Continue");
+            Console.ReadLine();
+        }
+
+        internal static void Division()
+        {
+            Console.WriteLine("Division Selected");
+            Console.WriteLine("Please enter the first number.");
+            var num1 = Console.ReadLine();
+            while (!double.TryParse(num1, out _))
+            {
+                Console.WriteLine("Please enter a valid number");
+                num1 = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{num1} is the first number.");
+            Console.WriteLine("Please enter the second number");
+            var num2 = Console.ReadLine();
+            while (!double.TryParse(num2, out _))
+            {
+                Console.WriteLine("Please enter a valid number");
+                num2 = Console.ReadLine();
+            }
+            Console.WriteLine($"{num2} is the second number");
+            var result = Convert.ToDouble(num1) / Convert.ToDouble(num2);
+            Console.WriteLine($"{num1} / {num2} is {result}");
+            Console.WriteLine("Press Enter to Continue");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+            Console.WriteLine("Welcome to the calculator app!");
+
+            while (!endApp)
+            {
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("a - Add");
+                Console.WriteLine("s - Subtract");
+                Console.WriteLine("m - Multiply");
+                Console.WriteLine("d - Divide");
+                Console.WriteLine("q - Quit");
+                Console.Write("Your option? ");
+                switch (Console.ReadLine().Trim().ToLower())
+                {
+                    case "a":
+                        Calculator.Addition();
+                        break;
+                    case "s":
+                        Calculator.Subtraction();
+                        break;
+                    case "m":
+                        Calculator.Multiplication();
+                        break;
+                    case "d":
+                        Calculator.Division();
+                        break;
+                    case "q":
+                        Console.WriteLine("Press Any Key To Exit");
+                        Console.ReadLine();
+                        endApp = true;
+                        break;
+                }
+                Console.Clear();
+            }
+        }
+        
+    }
+}
